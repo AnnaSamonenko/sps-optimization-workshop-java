ccf4788b6953e853acbec1e35ebabfa45d2d1e64
Anna Samonenko <annsamonenko97@gmail.com>
2019-06-23 15:52:06 +0300

    Init commit

---
+++ src/main/java/sps/optimization/workshop/SpsOptimizationWorkshopApplication.java
@@ -0,0 +1,13 @@
+package sps.optimization.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpsOptimizationWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpsOptimizationWorkshopApplication.class, args);
+	}
+
+}
